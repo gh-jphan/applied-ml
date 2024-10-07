6aa4083cb2ab78a3f821bb219c1719abc00cccb3
John Phan <jphan@guardanthealth.com>
2024-10-06 22:16:10 -0400

    final taxi notebook

--- New-York-Taxi-Analysis/get_data.sh
+++ New-York-Taxi-Analysis/get_data.sh
@@ -3,3 +3,5 @@
 mkdir -p ./data
 curl https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2019-01.parquet \
     -o ./data/yellow_tripdata_2019-01.parquet
+curl https://d37ci6vzurychx.cloudfront.net/misc/taxi_zone_lookup.csv \
+    -o ./data/taxi_zone_lookup.csv
